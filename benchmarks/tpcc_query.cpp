83283337cbc918e637c069f48a96cdd9bb310ce7
xuwenbin <xuwenbin@ruc.edu.cn>
2023-05-22 20:08:04 +0800

    fix error for only single server experiment

--- benchmarks/tpcc_query.cpp
+++ benchmarks/tpcc_query.cpp
@@ -238,7 +238,7 @@ BaseQuery * TPCCQueryGenerator::gen_new_order(uint64_t home_partition) {
     }
     ol_i_ids.insert(item->ol_i_id);
     item->ol_quantity = URand(1, 10);
-		if (r_mpr > g_mpr || g_num_wh == 1) {
+		if (r_mpr > g_mpr || g_node_cnt == 1) {
 			// home warehouse
 			item->ol_supply_w_id = query->w_id;
     } else {
